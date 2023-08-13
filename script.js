3fb0cece70df42187ecba186b973dec94330710a
Denisse Holgersen arredondo <132967474+Holgersend@users.noreply.github.com>
2023-08-12 23:33:23 -0500

    js and timer
    added the questions for the quiz

---
+++ script.js
@@ -0,0 +1,28 @@
+const questions = [
+    {
+        question: "Inside wich HTML elemt do we put the JavaScript?",
+        choices: ["1- <scripting>", "2- <js>", "3- <script>"],
+        answer: "3- <script>",
+    },
+    {
+        question: "Where is the correct place to insert a JavaScript?",
+        choices: ["1- the <head> section", "2- the <body> section", "3- both <head> and <body> section"],
+        answer: "2- the <doby> section",
+    },
+    {
+        question: "how do you create a function in JavaScript?",
+        choices: ["1- function = myFunction()", "2- function : myFunction()", "3- function myFunction() "],
+        answer: "1- function = myFunction()",
+    },
+    {
+        question: "How to write an IF statement for executing some code if i is NOT equal to 5?",
+        choices: ["1- if (i <> 5) ", "2- if (i != 5)", "3- if i <>5 "],
+        answer: "2- if (i != 5)",
+    },
+    {
+        question: "How do you declare a JavaScript variable?",
+        choices: ["1- var carName;", "2- variable carName;", "3- v carName; "],
+        answer: "1- var carName;",
+    },
+];
+
