b51438d1f364e9ae05e654eb4701773e319b2b7d
Denisse Holgersen arredondo <132967474+Holgersend@users.noreply.github.com>
2023-11-12 22:13:08 -0600

    script.js
    refactor old code, added checkAnsert endQuiz functions

--- script.js
+++ script.js
@@ -6,16 +6,25 @@ const questions = [
     },
     {
         question: "Where is the correct place to insert a JavaScript?",
-        choices: ["1- the <head> section", "2- the <body> section", "3- both <head> and <body> section"],
+        choices: [
+            "1- the <head> section",
+            "2- the <body> section",
+            "3- both <head> and <body> section",
+        ],
         answer: "2- the <body> section",
     },
     {
         question: "how do you create a function in JavaScript?",
-        choices: ["1- function = myFunction()", "2- function : myFunction()", "3- function myFunction() "],
+        choices: [
+            "1- function = myFunction()",
+            "2- function : myFunction()",
+            "3- function myFunction() ",
+        ],
         answer: "1- function = myFunction()",
     },
     {
-        question: "How to write an IF statement for executing some code if i is NOT equal to 5?",
+        question:
+            "How to write an IF statement for executing some code if i is NOT equal to 5?",
         choices: ["1- if (i <> 5) ", "2- if (i != 5)", "3- if i <>5 "],
         answer: "2- if (i != 5)",
     },
@@ -26,57 +35,77 @@ const questions = [
     },
 ];
 
-var box = document.getElementById('box');
-var startBtn = document.getElementsById("start-btn");
-var quiz = document.getElementById("questions");
-var questionElement = document.getElementById("question");
-var answer1 = document.getElementById("answer1");
-var answer2 = document.getElementById("answer2");
-var answer3 = document.getElementById("answer3");
+const startBtn = document.getElementById("start-btn");
+const timerElement = document.getElementById("timer");
+const questionsContainer = document.getElementById("questions");
+const questionElement = document.getElementById("question");
+const answerButtons = document.querySelectorAll("#questions button");
 
-var currentQuestionIndex = 0;
-
-var timeLeft = 10; // Time in seconds
-var timerInterval;
+let currentQuestionIndex = 0;
+let timeLeft = 60; // quiz time
 
 startBtn.addEventListener("click", startQuiz);
 
 function startQuiz() {
-    startBtn.style.display = "none";
-    quiz.style.display = "block";
-    startTimer();
-    displayQuestion();
+    document.getElementById("box").style.display = "none";
+    questionsContainer.style.display = "block";
+    setTime();
+    showQuestion();
 }
 
-function displayQuestion() {
-    if (currentQuestionIndex < questions.length) {
-        var currentQuestion = questions[currentQuestionIndex];
-        questionElement.textContent = currentQuestion.question;
-        answer1.textContent = currentQuestion.choices[0];
-        answer2.textContent = currentQuestion.choices[1];
-        answer3.textContent = currentQuestion.choices[2];
-    } else {
-        // Quiz ended
-        clearInterval(timerInterval);
-        quiz.style.display = "none";
-        box.innerHTML = "Quiz ended!";
-    }
-}
+function setTime() {
+    const timerInterval = setInterval(() => {
+        timeLeft--;
+        timerElement.textContent = `Time: ${timeLeft}`;
 
-function startTimer() {
-    timerInterval = setInterval(function () {
-        if (timeLeft > 0) {
-            timeLeft--;
-        } else {
+        if (timeLeft <= 0 || currentQuestionIndex === questions.length) {
             clearInterval(timerInterval);
-            nextQuestion();
+            endQuiz();
         }
     }, 1000);
 }
 
-function nextQuestion() {
+function showQuestion() {
+    const currentQuestion = questions[currentQuestionIndex];
+    questionElement.textContent = currentQuestion.question;
+
+    answerButtons.forEach((button, index) => {
+        button.textContent = currentQuestion.choices[index];
+        button.addEventListener("click", () => checkAnswer(index));
+    });
+}
+
+function checkAnswer(userChoice) {
+    if (userChoice === questions[currentQuestionIndex].correctAnswer) {
+        // Handle correct answer logic, update score if necessary
+    } else {
+        // Handle incorrect answer logic, subtract time if necessary
+        timeLeft -= 10; // Deduct 10 seconds for incorrect answers
+    }
+
     currentQuestionIndex++;
-    timeLeft -= 5; //deduct 5 second for incorrect answer
-    displayQuestion();
-    startTimer();
+    if (currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+        endQuiz();
+    }
+};
+
+function endQuiz() {
+    // Display the final score and allow the user to input initials
+    document.getElementById("questions").style.display = "none";
+    const finalScore = timeLeft > 0 ? timeLeft : 0;
+    document.getElementById("box").innerHTML = `
+      <h1>Quiz Over!</h1>
+      <p>Your final score is: ${finalScore}</p>
+      <label for="initials">Enter your initials:</label>
+      <input type="text" id="initials" />
+      <button id="submit-score" onclick="saveScore()">Submit Score</button>
+    `;
+}
+
+function saveScore() {
+    const initials = document.getElementById("initials").value;
+    // Handle saving the score, you can use localStorage or send it to a server
+    // You might want to redirect to a high scores page or do something else here
 };
